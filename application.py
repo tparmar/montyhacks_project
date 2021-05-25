f56ddecd6bcde4166701427b893846a01de43863
Teerthraj Parmar <68967593+tparmar@users.noreply.github.com>
2021-05-25 09:59:11 -0400

    commit on master

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import sqlite3
-from cs50 import SQL
 import flask
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -7,6 +6,13 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+#configure database
+sql_connect = sqlite3.connect("hospital.db")
+cursor = sql_connect.cursor()
 
 #configure application
 app = Flask(__name__)
@@ -16,3 +22,4 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
