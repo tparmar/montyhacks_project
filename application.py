45565661b25499fe82301f519740e831a8596aef
Teerthraj Parmar <68967593+tparmar@users.noreply.github.com>
2021-05-24 20:34:52 -0400

    commit on main

--- application.py
+++ application.py
@@ -1,1 +1,18 @@
+import sqlite3
+from cs50 import SQL
+import flask
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import os
 
+#configure application
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
