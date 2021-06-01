84e650fbcf47b3b2db1ee0680fbbbe760e32eb1e
Teerthraj Parmar <68967593+tparmar@users.noreply.github.com>
2021-06-01 16:30:30 -0400

    commit on main

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ import os
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
+from helpers import apology, login_required, usd
 
 #run this in your terminal before starting the flask application:
 #export set FLASK_APP=application
@@ -22,9 +23,54 @@ app = Flask(__name__)
 #Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    with sqlite3.connect("hospital.db") as con:
-        cursor = con.cursor()
-    return render_template("index.html")
+    """Log user in"""
 
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        #Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
