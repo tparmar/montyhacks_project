b8517808102572949574b66ac6ad6c693b810bf5
Teerthraj Parmar <68967593+tparmar@users.noreply.github.com>
2021-06-07 15:44:56 -0400

    commit

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ from mysql.connector import Error
 import pandas as pd
 from helpers import apology, login_required, usd
 
-#test
+
 #run this in your terminal before starting the flask application:
 #export set FLASK_APP=application
 #for you guys, its just set FLASK_APP=application
