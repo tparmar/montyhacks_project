26417c9e278d842b34da696faf1c76d426011c09
Nihaal Chadha <nihaalchadha@gmail.com>
2021-06-07 00:24:48 -0400

    commit on main

--- application.py
+++ application.py
@@ -14,6 +14,13 @@ from helpers import apology, login_required, usd
 #run this in your terminal before starting the flask application:
 #export set FLASK_APP=application
 #for you guys, its just set FLASK_APP=application
+
+# Nihaal- I need to do this to run it. It is the only way to change the environment to development in powershell.
+# cd montyhacks_project
+# $env:FLASK_APP = "application"
+# $env:FLASK_ENV = "development"
+# flask run
+
 #This will set the flask application
 con = sqlite3.connect("hospital.db", check_same_thread = False)
 cursor = con.cursor()
