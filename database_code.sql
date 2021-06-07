8d562e011cafd28327f12b91b87e52b7ee5bdfc5
Nihaal Chadha <nihaalchadha@gmail.com>
2021-06-07 15:24:03 -0400

    commit on main

--- database_code.sql
+++ database_code.sql
@@ -1,10 +1,4 @@
 -- SQLite
-CREATE TABLE people(
-    id INTEGER,
-    name TEXT NOT NULL,
-    birth NUMERIC,
-    PRIMARY KEY(id)
-);
 CREATE TABLE hospital(
     id INTEGER,
     name TEXT NOT NULL,
@@ -15,5 +9,8 @@ CREATE TABLE users(
     id INTEGER,
     username TEXT NOT NULL,
     hash TEXT NOT NULL,
-    type TEXT NOT NULL
+    type TEXT NOT NULL,
+    birth NUMERIC,
+    name TEXT NOT NULL,
+    gender TEXT NOT NULL
 );
